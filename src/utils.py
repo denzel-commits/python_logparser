989037da7eda97c8e2261d5ea4dabf754a06b459
python <python@ubuntu22.myguest.virtualbox.org>
2023-10-21 23:15:28 +0600

    Refactor: fix code-style

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ def get_logs(logdir):
     print("Logs found:", logfiles)
     return logfiles
 
+
 def json_dump(obj: list[dict], filename: str) -> None:
     with open(filename, "w", encoding="UTF-8") as f:
         json.dump(obj, f, ensure_ascii=False, indent=4)
